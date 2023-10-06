61423a9c92ee2853bb44848bdfc4ed1a6de62d2d
dipa-khadka <dipshanakhadka77@gmail.com>
2023-10-06 21:50:55 +0545

    adding login and register form

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,7 @@ import users.views as views
 
 urlpatterns = [
     path('', views.home, name="home_page"),
+    path('login/',views.user_login, name="login_page" ),
+    path('register/',views.user_register, name="register_page")
+   
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, template_name="index.html")
+
+
+def user_login(request):
+    return render(request, "login.html")
+ 
+def user_register(request):
+    return render(request, "register.html")
