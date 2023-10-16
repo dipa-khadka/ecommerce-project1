86159bb49cd2b31857f2f88d64160f7cd5490660
dipa-khadka <dipshanakhadka77@gmail.com>
2023-10-16 22:12:16 +0545

    changes

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('checkout/',views.checkout, name="checkout_page" ),
     path('order-summary/',views.order_summary, name="order_summary_page"),
     path('add-to-cart/<int:product_id/',views.add_to_cart, name="add_to_cart_page"),
-    path('remove-cart/<int:cart_id>', views.remove_from_cart, name="remove_from_cart")
+    path('remove-cart/<int:cart_id>', views.remove_from_cart, name="remove_from_cart"),
+    
 ]
